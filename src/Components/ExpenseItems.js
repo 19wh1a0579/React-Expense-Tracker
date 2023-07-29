3a1216aeb920a945e740b2dbf223e2e471882ac6
19wh1a0579 <19wh1a0579@bvrithyderabad.edu.in>
2023-07-29 09:51:40 +0530

    first Commit

---
+++ src/Components/ExpenseItems.js
@@ -0,0 +1,11 @@
+function ExpenseItems(){
+    return(
+        <div>
+        <h3>Expense Items</h3>
+        <h6>Food Rs 10</h6>
+        <h6>Petrol Rs 100</h6>
+        <h6>Movies Rs 200</h6>
+        </div>
+    )
+}
+export default ExpenseItems;
