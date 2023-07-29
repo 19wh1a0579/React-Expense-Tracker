1fe85a0e3beb19cab8efb595f627c7dffb3578c3
19wh1a0579 <19wh1a0579@bvrithyderabad.edu.in>
2023-07-29 15:35:20 +0530

    commit 3

--- src/Components/ExpenseItems.js
+++ src/Components/ExpenseItems.js
@@ -1,19 +1,16 @@
 import './ExpenseItems.css'
-function ExpenseItems(){
-    const expenseDate=new Date(2021,2,28);
-    const expenseTitle = 'Car insurance';
-    const expenseAmount = 295.67;
-    const locationOfExpenditure = "hyderabad";
+function ExpenseItems(props){
+    
 
     return(
         <div className="expense-item">
         <div>
-            {expenseDate.toISOString()}
+            {props.date.toISOString()}
         </div>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">{expenseAmount}</div>
-            <div>{locationOfExpenditure}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
+            <div>{props.location}</div>
         </div>
         </div>
     )
