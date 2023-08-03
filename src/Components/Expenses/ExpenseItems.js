cff4587c25afc3ca5a78df119e3546ed136fadc2
19wh1a0579 <19wh1a0579@bvrithyderabad.edu.in>
2023-08-03 08:49:00 +0530

    commit 6

--- src/Components/Expenses/ExpenseItems.js
+++ src/Components/Expenses/ExpenseItems.js
@@ -3,6 +3,9 @@ import Card from '../UI/Card';
 import './ExpenseItems.css';
 
 function ExpenseItem(props) {
+  const deleteExpense = () => {
+    console.log("Delete the expense!!!")
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -10,6 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={deleteExpense}>Delete Expense</button>
     </Card>
   );
 }
